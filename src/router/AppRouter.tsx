8148e3612422ca30ccb4f16abdbae31e1adde086
annyconfessor <anny.confessor@gmail.com>
2025-05-15 09:25:02 -0300

    chore: Remove unused files

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,12 +1,10 @@
 import { Routes as Router, Route } from 'react-router-dom'
 import Home from '../pages/Home'
-import NotFound from '../pages/NotFound'
 
 const AppRouter = () => {
   return(
     <Router>
       <Route path='/' element={<Home/>}></Route>
-      <Route path='/404' element={<NotFound/>}></Route>
     </Router>
   )
 }
